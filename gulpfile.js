fb21f1d99c2259a276fe46ce8709d7a277e6094a
Andrew Kelly <kellyaj@gmail.com>
2015-07-18 20:30:36 -0700

    cleans up gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -5,13 +5,14 @@ var gulp  = require('gulp'),
     neat  = require('node-neat').includePaths;
 
 var browserify = require('browserify');
-var reactify = require('reactify');
-var source = require('vinyl-source-stream');
-var notify = require('gulp-notify');
+var reactify   = require('reactify');
+var source     = require('vinyl-source-stream');
+var notify     = require('gulp-notify');
 
 
 var paths = {
   jsx: './src/js/**/*.jsx',
+  background: './src/js/background.js',
   scss: './src/styles/**/*.scss'
 };
 
@@ -19,43 +20,46 @@ var buildMessage = function(name, start) {
   return console.log("Built " + name + " in " + (Date.now() - start) + "ms")
 };
 
-gulp.task('build', function() {
-
-  var backgroundBundler = browserify({
-    debug: true
-  })
-  .require(require.resolve('./src/js/background.js'))
-
-  var backgroundBundle = function() {
-  var start = Date.now();
-  var destination = 'background_bundle.js';
-    backgroundBundler.bundle()
-    .pipe(source(destination))
-    .pipe(gulp.dest('build/js/bundles'))
-    .pipe(notify(function() { buildMessage(destination, start) }));
-  };
-
-  var bundler = browserify({
+gulp.task('buildComponents', function() {
+  var componentBundler = browserify({
     debug: true
   })
   .require(require.resolve('./src/js/wrangler.jsx'), { entry: true })
   .transform(reactify)
   .external('react');
 
-  var rebundle = function() {
+  var bundleComponents = function() {
     var start = Date.now();
     var destination = 'wrangler_bundle.js';
-    bundler.bundle()
+    componentBundler.bundle()
     .pipe(source(destination))
     .pipe(gulp.dest('build/js/bundles/'))
     .pipe(notify(function() { buildMessage(destination, start) }));
   };
 
-  rebundle();
-  backgroundBundle();
+  bundleComponents();
 });
 
-gulp.task('styles', function() {
+gulp.task('buildBackground', function() {
+  var backgroundBundler = browserify({
+    debug: true
+  })
+  .require(require.resolve('./src/js/background.js'))
+
+  var bundleBackground = function() {
+    var start = Date.now();
+    var destination = 'background_bundle.js';
+      backgroundBundler.bundle()
+      .pipe(source(destination))
+      .pipe(gulp.dest('build/js/bundles'))
+      .pipe(notify(function() { buildMessage(destination, start) }));
+  };
+
+  bundleBackground();
+});
+
+
+gulp.task('buildStyles', function() {
   var start = Date.now();
   gulp.src(paths.scss)
   .pipe(sass({includePaths: ['styles'].concat(neat)}))
@@ -64,8 +68,13 @@ gulp.task('styles', function() {
 });
 
 gulp.task('watch', function() {
-  gulp.watch(paths.jsx, ['build']);
-  gulp.watch(paths.scss, ['styles']);
+  gulp.watch(paths.jsx, ['buildComponents']);
+  gulp.watch(paths.background, ['buildBackground']);
+  gulp.watch(paths.scss, ['buildStyles']);
 });
 
-gulp.task('default', ['build', 'styles', 'watch'])
+gulp.task('build', function() {
+  gulp.start('buildBackground', 'buildComponents', 'buildStyles');
+});
+
+gulp.task('default', ['build', 'watch'])
